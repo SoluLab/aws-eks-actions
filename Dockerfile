242136a3d3b23b3254c37e8202839c4a41073b49
ravinayag <ravinayag@gmail.com>
2024-05-10 11:00:57 +0000

    updated version

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM alpine/k8s:1.28.9
 LABEL maintainer="Ravinayag <ravinayag@gmail.com>"
 
-
+RUN apk add --no-cache --update openssl curl ca-certificates
 RUN kubectl version --client
 
 RUN kubectl version --client
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
+CMD ["cluster-info"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,44 @@
 
 set -e
 
-export AWS_ACCESS_KEY_ID="$INPUT_AWS_ACCESS_KEY_ID"
-export AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
 
+echo "export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}"  >> $GITHUB_ENV
+echo "export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> $GITHUB_ENV
+
+echo "/usr/local/bin/kubectl" >> $GITHUB_PATH
+  
 echo "aws version"
 
 aws --version
 
+echo "kubectl version"
+kubectl version
+
+echo $AWS_REGION, $CLUSTER_NAME
+env 
+echo "GITHUB ENV"
+echo $GITHUB_ENV
+
 echo "Attempting to update kubeconfig for aws"
 
-aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
+
+aws eks --region "$AWS_REGION" update-kubeconfig --name "$CLUSTER_NAME"
+
+if [ ! -d "$HOME/.kube" ]; then
+    mkdir -p $HOME/.kube
+fi
+
+if [ ! -z "${KUBE_CONFIG}" ]; then
+        echo "$KUBE_CONFIG" | base64 -d > $HOME/.kube/config
+        kubectl get pods
+elif [ ! -z "${KUBE_TOKEN}" ]; then
+    kubectl config set-credentials cluster-admin --token="${KUBE_TOKEN}" > /dev/null
+else    
+    echo "No credentials found. Please provide KUBE_CONFIG or KUBE_TOKEN. Exiting..."
+    exit 1    
+fi    
+
+echo "kubectl version"
+
+
 kubectl "$@"
